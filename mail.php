db44f31c87c18f9b9e27dbf0969769905d11d45a
Сергей Манукянц <manukianc@odva.pro>
2020-02-06 11:03:10 +0300

    vacancy

--- mail.php
+++ mail.php
@@ -3,12 +3,12 @@ $name    = $_POST['name'];
 $vacancy = $_POST['vacancy'];
 $contact = $_POST['contact'];
 
-$to      = 'gona588a@gmail.com';
+$to      = 'isaniksa@gmail.com';
 $subject = 'Уведомление с сайта GTA5.RU';
-$headers = 'From: webmaster@gta5.ru' . "\r\n" .
+$headers = 'From: info@kalashnikovcom.ru' . "\r\n" .
 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n" .
 'Reply-To: gona588a@gmail.com' . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
-$message = 'Отклик с сайта\n Имя: {$name} \n Вакансии: {$vacancy} \n Контакты: {$cotnact}';
+$message = "Отклик с сайта\n Имя:'.$name. '\n Вакансии:'.$vacancy.' \n Контакты:'.$contact";
 // print_r($message);
 @mail($to, $subject, $message , $headers);
