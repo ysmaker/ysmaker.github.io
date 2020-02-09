581cf73ac2c70b642a8c9881fa18856b98fd6310
Сергей Манукянц <manukianc@odva.pro>
2020-02-09 17:45:03 +0300

    init

--- gulpfile.js
+++ gulpfile.js
@@ -65,7 +65,7 @@ gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
 gulp.task('imagemin', function() {
 	return gulp.src('app/img/**/*')
 	.pipe(cache(imagemin()))
-	.pipe(gulp.dest('dist/img')); 
+	.pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
